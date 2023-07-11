45577c964a4c2b1029ae61af57976dafbc30a970
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-07-11 19:56:23 +0700

    viewletter

--- easter-bunny/easter-bunny.py
+++ easter-bunny/easter-bunny.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-file_string = open('main.js', 'r').read()
+file_string = open('viewletter.js', 'r').read()
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/static/main.js')
+@app.route('/static/viewletter.js')
 def get_route():
     response = app.make_response(file_string)
     response.mimetype = 'text/javascript'
