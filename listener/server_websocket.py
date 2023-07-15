231e421c637c53dacd9e3fe3c367b66fedddc4f4
emir.shamsuddin <emir.shamsuddin@ui.ac.id>
2023-07-15 20:11:21 +0700

    m

--- listener/server_websocket.py
+++ listener/server_websocket.py
@@ -5,7 +5,7 @@ async def handle_code(websocket, path):
     code = await websocket.recv()
     print("Received code:", code)
 
-start_server = websockets.serve(handle_code, "localhost", 8080)
+start_server = websockets.serve(handle_code, "0.0.0.0", 12345)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
