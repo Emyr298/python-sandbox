02b92671c2030109b341555a91c113b374513ac9
Emyr298 <emi.ru298@gmail.com>
2023-07-28 16:53:52 +0700

    feat: attacker.py

---
+++ attacker-py/attacker.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/redirect')
+def get_route():
+    url = request.args.get('url', None)
+    if url is None:
+        url = request.headers.get('url')
+    if url is None:
+        return 'Invalid url', 400
+    
+    return redirect(url)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+    
